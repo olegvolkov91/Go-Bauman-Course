6c4a72d47d3fe38ad36c8546334e24d91fa6ae6d
Oleg Volkov <voa91ua@gmail.com>
2023-02-09 23:31:07 +0200

    add basic routes and empty handlers

---
+++ Basic_http_client/basic_pizza_rest/go.mod
@@ -0,0 +1,5 @@
+module github.com/olegvolkov91/Go-Bauman-Course/tree/main/Basic_http_client/basic_pizza_rest
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- Basic_http_client/basic_pizza_rest/main.go
+++ Basic_http_client/basic_pizza_rest/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
@@ -11,5 +12,15 @@ var (
 
 func main() {
 	log.Println("Starting pizza rest api ...")
+	router := mux.NewRouter()
+	router.HandleFunc("/pizza", GetAllPizzas).Methods(http.MethodGet)
+	router.HandleFunc("/pizza/{id}", GetPizzaById).Methods(http.MethodGet)
+	log.Println("configured successfully")
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func GetPizzaById(w http.ResponseWriter, req *http.Request) {
+}
+
+func GetAllPizzas(w http.ResponseWriter, req *http.Request) {
 }
